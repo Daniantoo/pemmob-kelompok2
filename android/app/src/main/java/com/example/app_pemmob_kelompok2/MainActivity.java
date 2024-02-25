9274f1155e30dacf7318766929ddab1540aa7fd5
Daniantoo <dion.22058@mhs.unesa.ac.id>
2024-02-25 19:51:56 +0700

    Upload Code Dart Android Studio

---
+++ android/app/src/main/java/com/example/app_pemmob_kelompok2/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.app_pemmob_kelompok2;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'ChizPic',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/camera.png'),
+          ),
+        ),
+        backgroundColor: const Color(0xFFE1BE66),
+      ),
+    ),
+  );
+}
